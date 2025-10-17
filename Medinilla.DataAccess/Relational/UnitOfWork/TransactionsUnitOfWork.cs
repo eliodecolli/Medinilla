249e35b8f55073b12c3f1c60d26c5b8669ed7428
Elio Decolli <eliodecolli@gmail.com>
2025-10-17 16:33:30 +0200

    changed TUoW

--- Medinilla.DataAccess/Relational/UnitOfWork/TransactionsUnitOfWork.cs
+++ Medinilla.DataAccess/Relational/UnitOfWork/TransactionsUnitOfWork.cs
@@ -12,16 +12,18 @@ public sealed class TransactionsUnitOfWork(MedinillaOcppDbContext context)
     private IRepository<TransactionEvent> _transactionRepository = new GenericRepository<TransactionEvent>(context);
     private IRepository<TransactionSnapshot> _snapshotRepository = new GenericRepository<TransactionSnapshot>(context);
 
-    public async Task<TransactionEvent> RegisterTransaction(TransactionEvent transaction, Models.Authorization.IdToken? idToken)
+    public async Task<TransactionEvent> RegisterTransaction(TransactionEvent transaction, IdToken? idToken)
     {
-        if (idToken is not null)
+        if (idToken is null)
         {
-            transaction.IdTokenId = idToken.Id;
+            return await _transactionRepository.Create(transaction);
+        }
+        
+        transaction.IdTokenId = idToken.Id;
 
-            if (!idToken.IsUnderTx)
-            {
-                idToken.IsUnderTx = true;
-            }
+        if (!idToken.IsUnderTx)
+        {
+            idToken.IsUnderTx = true;
         }
 
         return await _transactionRepository.Create(transaction);
@@ -35,35 +37,9 @@ public sealed class TransactionsUnitOfWork(MedinillaOcppDbContext context)
 
     public async Task<IdToken?> TryGetIdTokenForTransaction(string transactionId)
     {
-        var query = await _transactionRepository.Filter(tx => tx.IdTokenId is not null);
+        var query = await _transactionRepository.Filter(tx => tx.TransactionId == transactionId && 
+                                                              tx.IdTokenId is not null);
         return query.FirstOrDefault()?.IdToken;
-    }
-
-    public async Task<TransactionEvent?> TryGetLatestTransaction(string transactionId)
-    {
-        var relatedTransactions = await _transactionRepository.Filter(x => x.TransactionId == transactionId);
-        return await Task.FromResult(relatedTransactions.OrderByDescending(x => x.SeqNo).FirstOrDefault());
-    }
-
-    public async Task<int[]> TryGetMissingTransactionsForIncomingEvent(IEnumerable<TransactionEvent> relatedTransactions, int incomingSeqNo)
-    {
-        var retval = new HashSet<int>();
-
-        var seqNos = await Task.FromResult(relatedTransactions.Select(x => x.SeqNo).ToList());
-        seqNos.Add(incomingSeqNo);
-
-        var lastSeqNo = seqNos.OrderBy(x => x).LastOrDefault();
-
-        for (var i = 1; i <= lastSeqNo; i++)
-        {
-            if (!seqNos.Contains(i))
-            {
-                // yeah we're already past it
-                retval.Add(i);
-            }
-        }
-
-        return [.. retval];
     }
 
     public async Task FinalizeSnapshot(TransactionEvent? firstEvent, TransactionEvent lastEvent, TransactionConsumption? consumption)
