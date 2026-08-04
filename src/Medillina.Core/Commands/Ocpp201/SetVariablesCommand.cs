7c0c91f95f981f026ec5a869ece96829c53adafa
Elio <eliodecolli@gmail.com>
2026-08-04 23:32:05 +0200

    Update SetVariablesCommand.cs

--- src/Medillina.Core/Commands/Ocpp201/SetVariablesCommand.cs
+++ src/Medillina.Core/Commands/Ocpp201/SetVariablesCommand.cs
@@ -2,8 +2,6 @@
 using Medinilla.Infrastructure.WAMP;
 using Microsoft.Extensions.Logging;
 using Medinilla.DataTypes.Contracts.Common;
-using System.Text.Json;
-
 
 namespace Medinilla.Core.Commands.Ocpp201;
 
@@ -28,19 +26,65 @@ internal sealed class SetVariablesCommand(ILogger<SetVariablesCommand> log) : IO
             return;
         }
 
+        var accepted = 0;
+        var rebootRequired = 0;
+        var notAccepted = 0;
+
         foreach (var varResp in response.SetVariableResult)
         {
-            if (varResp.AttributeStatus != SetVariableStatusEnum.Accepted)
+            var evseId = varResp.Component.Evse?.Id;
+            var connectorId = varResp.Component.Evse?.ConnectorId;
+
+            switch (varResp.AttributeStatus)
             {
-                log.LogError("(SetVariables) {mi}: Set variable result for variable {vname} failed: {s}. Component details: {cd}",
-                    result.MessageId, varResp.Variable.Name, varResp.AttributeStatus.ToString(), GetComponentDetails(varResp.Component));
+                case SetVariableStatusEnum.Accepted:
+                    accepted++;
+                    log.LogDebug(
+                        "(SetVariables) {mi}: accepted {vname} on {cname}.{cinst} (evse={evseId}, connector={connectorId}, attr={attr})",
+                        result.MessageId,
+                        varResp.Variable.Name,
+                        varResp.Component.Name,
+                        varResp.Component.Instance,
+                        evseId,
+                        connectorId,
+                        varResp.AttributeType);
+                    break;
+
+                case SetVariableStatusEnum.RebootRequired:
+                    rebootRequired++;
+                    log.LogInformation(
+                        "(SetVariables) {mi}: reboot required for {vname} on {cname}.{cinst} (evse={evseId}, connector={connectorId}, attr={attr})",
+                        result.MessageId,
+                        varResp.Variable.Name,
+                        varResp.Component.Name,
+                        varResp.Component.Instance,
+                        evseId,
+                        connectorId,
+                        varResp.AttributeType);
+                    break;
+
+                default:
+                    notAccepted++;
+                    log.LogWarning(
+                        "(SetVariables) {mi}: {status} for {vname} on {cname}.{cinst} (evse={evseId}, connector={connectorId}, attr={attr})",
+                        result.MessageId,
+                        varResp.AttributeStatus,
+                        varResp.Variable.Name,
+                        varResp.Component.Name,
+                        varResp.Component.Instance,
+                        evseId,
+                        connectorId,
+                        varResp.AttributeType);
+                    break;
             }
         }
-    }
 
-    private string GetComponentDetails(Component component)
-    {
-        // take the easy way
-        return JsonSerializer.Serialize(component);
+        log.LogInformation(
+            "(SetVariables) {mi}: {ok}/{total} accepted, {rr} reboot-required, {failed} not accepted",
+            result.MessageId,
+            accepted,
+            response.SetVariableResult.Count,
+            rebootRequired,
+            notAccepted);
     }
 }
